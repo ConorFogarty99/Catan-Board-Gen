0c26118dc81efb2f9a2c08f2df065538f7b52b4d
ConorFogarty99 <fogartyconor3@gmail.com>
2023-04-14 01:29:31 +0100

    Add adjacency checks and started adding rules for tiles
    Signed-off-by: ConorFogarty99 <fogartyconor3@gmail.com>

--- main.py
+++ main.py
@@ -28,6 +28,54 @@ def translate_board(board):
     return board
 
 
+def check_board(board):
+    # Loop over each row
+    for row in range(len(board)):
+        # Loop over each tile in the row
+        for col in range(len(board[row])):
+            # Get the current tile
+            current_tile = board[row][col]
+
+            # Get the adjacent tiles
+            if row % 2 == 0:
+                # Even row
+                top_left = (row - 1, col)
+                top_right = (row - 1, col + 1)
+                left = (row, col - 1)
+                right = (row, col + 1)
+                bottom_left = (row + 1, col)
+                bottom_right = (row + 1, col + 1)
+            else:
+                # Odd row
+                top_left = (row - 1, col - 1)
+                top_right = (row - 1, col)
+                left = (row, col - 1)
+                right = (row, col + 1)
+                bottom_left = (row + 1, col - 1)
+                bottom_right = (row + 1, col)
+
+            adjacent_tiles = [top_left, top_right, left, right, bottom_left, bottom_right]
+            adjacent_resources = [board[row][col] for row, col in adjacent_tiles if
+                                  0 <= row < len(board) and 0 <= col < len(board[row])]
+
+            # print(current_tile, adjacent_resources)
+
+            # Rule 1: No 2 brick or stone tiles next to e/o
+            if current_tile[0] == 'brick' or current_tile[0] == 'ore':
+                same_resource_adjacent = False
+                for adj_tile in adjacent_resources:
+                    if adj_tile[0] == current_tile[0]:
+                        same_resource_adjacent = True
+                        break
+                if same_resource_adjacent:
+                    print("Rule 1: No 2 brick or stone tiles next to e/o violation at", (row, col))
+                    print("Regenerating the board...")
+                    board_layout()
+                    return
+
+    return True
+
+
 def board_layout():
     resource_list = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5]
     numbers_list = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
@@ -50,8 +98,14 @@ def board_layout():
                 numbers_list.remove(number)
 
     board_texted = translate_board(board)
-    print(board_texted)
+    check_board(board_texted)
 
 
 if __name__ == '__main__':
-    board_layout()
+    board_generated = False
+    while not board_generated:
+        board = board_layout()
+        if board is not None:
+            board_generated = True
+    # board_texted = translate_board(board)
+    print(board)
